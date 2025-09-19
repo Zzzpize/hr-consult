c2abaf6a47361be47ca6dbd8ac0e43fb0abf7a9f
Zzzpize <zzzpize@yandex.com>
2025-09-19 20:08:48 +0500

    Docker & frontend/app.py added; requiremets.txt changed

---
+++ frontend/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Talent Navigator AI",
+    page_icon="🚀"
+)
+
+st.title("Добро пожаловать в HR Navigator AI!")
+st.write("Сервис в процессе разработки. Скоро здесь будет круто!")
