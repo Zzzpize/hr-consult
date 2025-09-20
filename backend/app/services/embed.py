3cf644eed908f83fe3abe6920eb7e7a37b94f8a8
dmitryz1024 <dimonthly@gmail.com>
2025-09-20 21:40:12 +0300

    Some tries of using embed-model

--- backend/app/services/embed.py
+++ backend/app/services/embed.py
@@ -1,31 +1,36 @@
+import json
+import numpy as np
 from openai import OpenAI
 
 API_KEY = "sk-LqCf11ej9dj70ObUG956eQ"
 BASE_URL = "https://llm.t1v.scibox.tech/v1"
+client = OpenAI(api_key = API_KEY, base_url = BASE_URL)
 
-texts_to_embed = [
-    "Хакатон — это марафон для программистов.",
-    "Команда пишет код всю ночь.",
-    "Солнечная погода на пляже."
-]
+def get_embedding(text: str) -> list[float]:
+    response = client.embeddings.create(
+        model = "bge-m3",
+        input = text
+    )
+    return response.data[0].embedding
 
-def exchange(texts: list[str]) -> list[list[float]]:
-    try:
-        client = OpenAI(api_key = API_KEY, base_url = BASE_URL)
-        print(f"Отправляю {len(texts_to_embed)} текстов на векторизацию моделью 'bge-m3'...")
-        emb_response = client.embeddings.create(
-            model = "bge-m3",
-            input = texts_to_embed
-        )
-        num_embeddings = len(emb_response.data)
-        print(f"Получено эмбеддингов: {num_embeddings}")
-        if num_embeddings > 0:
-            vector_dimension = len(emb_response.data[0].embedding)
-            print(f"Размерность каждого вектора (эмбеддинга): {vector_dimension}")
-            print(f"Пример части первого вектора: {emb_response.data[0].embedding[:5]}")
-        if num_embeddings == len(texts_to_embed):
-            print("Тест эмбеддинг-модели прошел успешно!")
-        else:
-            print("Тест прошел, но количество эмбеддингов не совпадает с количеством текстов!")
-    except Exception as e:
-        print(f"\n--- ПРОИЗОШЛА ОШИБКА ---{e}")
+def cosine_similarity(vec1, vec2):
+    v1, v2 = np.array(vec1), np.array(vec2)
+    return np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+
+with open("backend/app/services/career_plans.json", "r") as f: career_plans = json.load(f)
+
+with open("backend/app/services/user_profile.json", "r") as f: user_profile = json.load(f)
+
+user_text = " ".join(user_profile["skills"])
+user_vec = get_embedding(user_text)
+
+best_match, best_score = None, -1
+for plan in career_plans:
+    plan_text = f"{plan['target_role']} {plan['description']}"
+    plan_vec = get_embedding(plan_text)
+    score = cosine_similarity(user_vec, plan_vec)
+    if score > best_score:
+        best_match, best_score = plan, score
+
+print("Лучший план:", best_match["target_role"])
+print("Score:", best_score)
